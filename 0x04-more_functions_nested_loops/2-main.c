7cee8c7bad712a68785721591fd41f0c4afece20
shacky-me <meshackkiprono12@gmail.com>
2023-09-14 04:48:21 -0700

    2-mul

--- 0x04-more_functions_nested_loops/2-main.c
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -6,7 +6,6 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
 	printf("%d\n", mul(98, 1024));
--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
-
 /**
- * mul - Multiplies two integers
- * @x: The first number to multiply
- * @y: The second number to multiply with first number
- * Return: The result of multiplication between x param and y param
+ * mul - function that multiplies 2 integers
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: a * b
  */
 
 int mul(int a, int b)
