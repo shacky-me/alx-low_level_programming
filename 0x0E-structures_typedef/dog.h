0cd2ea726082cc5d34369f54162dae5ceb075714
shacky-me <meshackkiprono12@gmail.com>
2023-10-09 10:50:13 -0700

    A dog is the only thing on earth that loves you more than you love yourself

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,28 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _HEADER_
+#define _HEADER_
 
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+/**
+  * struct dog - Dog attributes
+  * @name: The name of the dog.
+  * @age: The age of the dog.
+  * @owner: The owner of the dog.
+  *
+  * Description: The attributes of a dog.
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
-#endif /* DOG_H */
+/**
+ * dog_t - Typedef for dog structure
+ */
+typedef struct dog dog_t;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
