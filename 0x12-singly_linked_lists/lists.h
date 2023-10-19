a1b8e361a57e0dcdff5d176e55dcb1507e92087b
shacky-me <meshackkiprono12@gmail.com>
2023-10-19 13:34:53 -0700

    Print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,8 +3,8 @@
 
 /**
  * struct list_s - singly linked list
- * @str: string - (malloc string)
- * len: length of the string
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
  * @next: points to the next node
  *
  * Description: singly linked list node structure
