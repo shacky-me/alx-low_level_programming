ef8ae20a9fffd2bbb601ed0d68e9ec5bc7185237
shacky-me <meshackkiprono12@gmail.com>
2023-10-24 13:46:56 -0700

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,8 +4,8 @@
 
 /**
  * print_listint - Prints all elements of a listint_t list.
- *@h: Pointer to the head of the list.
- *Return: The number of nodes in the list.
+ * @h: Pointer to the head of the list.
+ * Return: The number of nodes in the list.
  */
 
 size_t print_listint(const listint_t *h)
