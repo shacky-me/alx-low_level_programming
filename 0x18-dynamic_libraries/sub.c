9e00732b7af41762e234afa71e32ed739ed603d4
shacky-me <meshackkiprono12@gmail.com>
2024-04-10 10:18:15 -0700

    executed files in a row

---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ * sub - subtract two integers
+ * @a - first int
+ * @b - second int
+ * Return: Subtracted Final Result
+ */
+
+int add(int a, int b)
+{
+	return (a - b);
+}
